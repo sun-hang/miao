3cb552bac3d3895d9af07f875672c14914f9f385
sun-hang <1844528595@qq.com>
2020-11-13 17:21:21 +0800

    _2020_11_13_17_21

---
+++ src/root/imgMiddleware.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { Request, Response, NextFunction } from 'express'
+import url from 'url';
+export default (req: Request, res: Response, next: NextFunction) => {
+    const extName = path.extname(req.path)
+    let referer = req.headers.referer || ''
+    const origin = req.headers.host || '';
+    if (!['.jpg', '.png', '.jpeg', '.gif'].includes(extName)) {
+        next();
+        return
+    }
+    if (referer) {
+        referer = url.parse(referer).host ? url.parse(referer).host as string: '';
+    }
+    if (referer && origin !== referer) {
+        req.url = '/img/1.jpg'
+    }
+    next()
+}
