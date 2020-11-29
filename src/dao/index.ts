2572b6a26da5e281aea665651641d4dcc01a8741
feng <57206049+sun-hang@users.noreply.github.com>
2020-11-30 03:57:47 +0800

    Update index.ts

--- src/dao/index.ts
+++ src/dao/index.ts
@@ -1,7 +1,7 @@
 // 主文件
 import {Sequelize} from 'sequelize';
-
-module.exports = new Sequelize('miao',"root","root",{
+let pass = "980529Ssf.";
+module.exports = new Sequelize('miao',"root",pass,{
     host:"127.0.0.1",
     port:3306,
     dialect:"mysql",
