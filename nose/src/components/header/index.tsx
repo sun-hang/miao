e4f7578efd2a0091c8dd324478b347866af13f05
sun-hang <1844528595@qq.com>
2020-12-09 15:30:13 +0800

    _2020_12_9_15_30

--- nose/src/components/header/index.tsx
+++ nose/src/components/header/index.tsx
@@ -13,16 +13,15 @@ export default function index() {
                         <img className="log-img" src={logSrc} alt="" />
                     </NavLink>
                 </Col>
-                <Col className="nav-list-box">
+                <Col className="nav-list-box" push={10}>
                     <Row>
                         <Col>
                             <ul className="nav-box">
-                                <li className="nav-item"><NavLink to='/'><p>首页</p><p>HOME</p>
-                                </NavLink></li>
-                                <li className="nav-item"><NavLink to='/'><p>产品</p><p>PRODUCT</p></NavLink></li>
-                                <li className="nav-item"><NavLink to='/'><p>新闻</p><p>NEWS</p></NavLink></li>
-                                <li className="nav-item"><NavLink to='/'><p>关于</p><p>ABOUT</p></NavLink></li>
-                                <li className="nav-item"><NavLink to='/'><p>联系</p><p>CONTACT</p></NavLink></li>
+                                <li className="nav-item"><NavLink exact activeClassName="nav-action" to='/'><p>首页</p><p>HOME</p></NavLink></li>
+                                <li className="nav-item"><NavLink exact activeClassName="nav-action"  to='/product'><p>产品</p><p>PRODUCT</p></NavLink></li>
+                                <li className="nav-item"><NavLink exact activeClassName="nav-action" to='/news'><p>新闻</p><p>NEWS</p></NavLink></li>
+                                <li className="nav-item"><NavLink exact activeClassName="nav-action" to='/about'><p>关于</p><p>ABOUT</p></NavLink></li>
+                                <li className="nav-item"><NavLink exact activeClassName="nav-action" to='/contact'><p>联系</p><p>CONTACT</p></NavLink></li>
                             </ul>
                         </Col>
                         <Col>
